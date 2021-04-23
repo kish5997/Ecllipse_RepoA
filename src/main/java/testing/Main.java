168788542a2b6aefe54124e25c6f11da9e8e104d
kishore <kish5997@gmail.com>
2021-04-23 18:33:14 +0530

    testmm

--- src/main/java/testing/Main.java
+++ src/main/java/testing/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("test");
+		System.out.println(" testing 08");
 
+	}
+	void master() {
+		System.out.println(" testing 02");
 	}
 
 }
